4aa4942db8ceeddc52236022c87f2ac66552e728
yananofnanjing <71796112+yananofnanjing@users.noreply.github.com>
2020-10-26 17:51:16 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@
  */
 
 const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
-/*let number1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,];*/
 
 
 /**
@@ -18,46 +17,37 @@ function makeArray(n) {
   for (let i=1; i<n; i++) {
     array.push(i);
   }
-  /*array.toString();
-  confirm(array);*/
   return array;
 }
 
 function allowedOrNot(x, allowed) {
-  /*x = x.toLocaleUpperCase();*/
   let notallowed = [];
   for (let a of x) {
-    /*confirm(a);*/
     if (allowed.includes(a)) {
-      /*confirm(a);*/
       continue;
     }
     else {
       notallowed.push(a);
-      /*return false;*/
     }
   }
   let l = notallowed.length;
-  /*confirm(l.toString());*/
   if (l === 0) {
-    /*confirm(notallowed.toString());*/
     return true;
   }
   else {
-    /*confirm(notallowed.toString());*/
     return notallowed;
   }
 }
 
 function question1(input) {
-  if (input === `encode`) {
+  if (input === `kóða`) {
     let action = input;
     let n = Number(prompt(`Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]`));
     if (number.includes(n)) {
       input = prompt(`Gefðu upp strenginn sem á að ${action} með hliðrun ${n}`);
       if (input === ``) {
         confirm (`Þú gafst ekki upp streng. Reyndu aftur.`)
-        input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`);
+        input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`);
         question1(input);
       } else {
         input = input.toUpperCase();
@@ -65,28 +55,27 @@ function question1(input) {
         if (invalid === true) {
           result = encode(input, n);
           confirm(result);
-          /*alert(`i'm hungry`);*/
         }
         else {
           confirm(`Þú gafst upp stafi sem ekki er hægt að ${action}: ${invalid.join(', ')}. Reyndu aftur.`);
-          input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`);
+          input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`);
           question1(input);
         }
       }
     }
     else {
       confirm(`${n} er ekki heiltala á bilinu [1, 31]. Reyndu aftur.`)
-      input= (prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`));
+      input= (prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`));
       question1(input);
     }
-  } else if (input===`decode`) {
+  } else if (input===`afkóða`) {
     let action = input;
     let n = Number(prompt(`Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]`));
     if (number.includes(n)) {
       input = prompt(`Gefðu upp strenginn sem á að ${action} með hliðrun ${n}`);
       if (input === ``) {
         confirm(`Þú gafst ekki upp streng. Reyndu aftur.`)
-        input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`);
+        input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`);
         question1(input);
       } else {
         input = input.toUpperCase();
@@ -94,23 +83,22 @@ function question1(input) {
         if (invalid === true) {
           result = decode(input, n);
           confirm(result);
-          /*alert(`i'm thirsty`);*/
         }
         else {
           confirm(`Þú gafst upp stafi sem ekki er hægt að ${action}: ${invalid.join(', ')}. Reyndu aftur.`);
-          input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`);
+          input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`);
           question1(input);
         }
       }
     }
     else {
       confirm(`${n} er ekki heiltala á bilinu [1, 31]. Reyndu aftur.`)
-      input= prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`);
+      input= prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`);
       question1(input);
     }
   } else {
     confirm(`Veit ekki hvaða aðgerð „${input}“ er. Reyndu aftur.`)
-    input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`);
+    input = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`);
     question1(input);
   }
 }
@@ -118,9 +106,7 @@ function question1(input) {
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
 start();
 let number = makeArray(32);
-/*let b = prompt (`capitalized icelandic letter`);
-allowedOrNot(b, LETTERS);*/
-let a = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „encode“ eða „decode“`);
+let a = prompt(`Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða“`);
 question1(a);
 
 /**
